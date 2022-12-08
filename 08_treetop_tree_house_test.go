774775e0da516077d06132247f4bdbdfffbec736
Bernd Hartzer <berndhartzer@gmail.com>
2022-12-08 20:58:21 +1000

    day 8 part 2

--- 08_treetop_tree_house_test.go
+++ 08_treetop_tree_house_test.go
@@ -90,6 +90,72 @@ func treeTopTreeHousePartOne(input []string) int {
 	return count
 }
 
+func treeTopTreeHousePartTwo(input []string) int {
+	highestScenic := 0
+	columns := make([]string, len(input[0]))
+
+	for y := 1; y < len(input)-1; y++ {
+		for x := 1; x < len(input[y])-1; x++ {
+			height := input[y][x]
+
+			// Get cached column, or calculate
+			column := func() string {
+				if len(columns[x]) != 0 {
+					return columns[x]
+				}
+
+				s := ""
+				for j := 0; j < len(input); j++ {
+					s += string(input[j][x])
+				}
+				columns[x] = s
+				return s
+			}()
+
+			scores := make([]int, 4)
+
+			up := column[:y]
+			for j := len(up) - 1; j >= 0; j-- {
+				scores[0]++
+				if up[j] >= height {
+					break
+				}
+			}
+
+			down := column[y+1:]
+			for _, h := range down {
+				scores[1]++
+				if byte(h) >= height {
+					break
+				}
+			}
+
+			left := input[y][:x]
+			for j := len(left) - 1; j >= 0; j-- {
+				scores[2]++
+				if left[j] >= height {
+					break
+				}
+			}
+
+			right := input[y][x+1:]
+			for _, h := range right {
+				scores[3]++
+				if byte(h) >= height {
+					break
+				}
+			}
+
+			scenic := scores[0] * scores[1] * scores[2] * scores[3]
+			if scenic > highestScenic {
+				highestScenic = scenic
+			}
+		}
+	}
+
+	return highestScenic
+}
+
 func TestDayEight(t *testing.T) {
 	type testConfig struct {
 		input     []string
@@ -140,5 +206,26 @@ func TestDayEight(t *testing.T) {
 		}
 
 		runTests(t, tests, treeTopTreeHousePartOne)
+	})
+
+	t.Run("part two", func(t *testing.T) {
+		tests := map[string]testConfig{
+			"test_1": {
+				input: []string{
+					"30373",
+					"25512",
+					"65332",
+					"33549",
+					"35390",
+				},
+				expected: 8,
+			},
+			"solution": {
+				input:     input,
+				logResult: true,
+			},
+		}
+
+		runTests(t, tests, treeTopTreeHousePartTwo)
 	})
 }
