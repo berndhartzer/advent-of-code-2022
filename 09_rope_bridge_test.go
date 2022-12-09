27160f1b4027994d4efd6a64e353477e5c43932b
Bernd Hartzer <berndhartzer@gmail.com>
2022-12-09 18:12:45 +1000

    day 9 part 2

--- 09_rope_bridge_test.go
+++ 09_rope_bridge_test.go
@@ -43,7 +43,7 @@ func ropeBridgePartOne(input []string) int {
 			yDist := head.y - tail.y
 
 			switch {
-			case (abs(xDist) > 0 && abs(yDist)> 0) && (abs(xDist) > 1 || abs(yDist) > 1):
+			case (abs(xDist) > 0 && abs(yDist) > 0) && (abs(xDist) > 1 || abs(yDist) > 1):
 				// diagonal move
 				if xDist > 0 {
 					tail.x += 1
@@ -67,6 +67,85 @@ func ropeBridgePartOne(input []string) int {
 			}
 
 			tailPos := fmt.Sprintf("%d,%d", tail.x, tail.y)
+			visited[tailPos] = true
+		}
+	}
+
+	return len(visited)
+}
+
+func ropeBridgePartTwo(input []string) int {
+	rope := []*ropeKnot{
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+		{},
+	}
+	visited := map[string]bool{}
+
+	for _, line := range input {
+		dist, err := strconv.Atoi(line[2:])
+		if err != nil {
+			panic("failed to convert string to int")
+		}
+
+		xMove := 0
+		yMove := 0
+		switch line[0] {
+		case 'U':
+			yMove = -1
+		case 'D':
+			yMove = 1
+		case 'L':
+			xMove = -1
+		case 'R':
+			xMove = 1
+		}
+
+		for i := 0; i < dist; i++ {
+			rope[0].x += xMove
+			rope[0].y += yMove
+
+			for j := 1; j < len(rope); j++ {
+				prevKnot := rope[j-1]
+				thisKnot := rope[j]
+
+				xDist := prevKnot.x - thisKnot.x
+				yDist := prevKnot.y - thisKnot.y
+
+				switch {
+				case (abs(xDist) > 0 && abs(yDist) > 0) && (abs(xDist) > 1 || abs(yDist) > 1):
+					// diagonal move
+					if xDist > 0 {
+						thisKnot.x += 1
+					} else {
+						thisKnot.x -= 1
+					}
+					if yDist > 0 {
+						thisKnot.y += 1
+					} else {
+						thisKnot.y -= 1
+					}
+
+				case xDist > 1:
+					thisKnot.x += 1
+				case xDist < -1:
+					thisKnot.x -= 1
+				case yDist > 1:
+					thisKnot.y += 1
+				case yDist < -1:
+					thisKnot.y -= 1
+				}
+
+			}
+
+			tailPos := fmt.Sprintf("%d,%d", rope[9].x, rope[9].y)
 			visited[tailPos] = true
 		}
 	}
@@ -127,5 +206,42 @@ func TestDayNine(t *testing.T) {
 		}
 
 		runTests(t, tests, ropeBridgePartOne)
+	})
+
+	t.Run("part two", func(t *testing.T) {
+		tests := map[string]testConfig{
+			"test_1": {
+				input: []string{
+					"R 4",
+					"U 4",
+					"L 3",
+					"D 1",
+					"R 4",
+					"D 1",
+					"L 5",
+					"R 2",
+				},
+				expected: 1,
+			},
+			"test_2": {
+				input: []string{
+					"R 5",
+					"U 8",
+					"L 8",
+					"D 3",
+					"R 17",
+					"D 10",
+					"L 25",
+					"U 20",
+				},
+				expected: 36,
+			},
+			"solution": {
+				input:     input,
+				logResult: true,
+			},
+		}
+
+		runTests(t, tests, ropeBridgePartTwo)
 	})
 }
